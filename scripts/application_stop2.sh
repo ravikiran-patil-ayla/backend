f58bdbdf23c1da552a2bcb00d94b1d47a5857899
ravikiran-patil-ayla <ravikiran.r@aylanetworks.com>
2022-07-20 16:07:35 +0530

    before install ci configure 2

--- scripts/application_stop2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Stopping existing node servers
-echo "Stopping any existing node servers"
-# pkill node
-
-# ApplicationStop:
-    # - location: scripts/application_stop.sh
-    #   timeout: 300
-    #   runas: ec2-user
